b5ba85402d1ebfeaadaa5bf887757cea066016af
shellingford330 <pg.econ4869@gmail.com>
2021-12-25 02:55:25 +0900

    feat: prettier / husky / lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "**/*.ts?(x)": (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(" --file ")}`,
+  "**/*.{js,jsx,ts,tsx,json,css}": "prettier --write --ignore-path .gitignore",
+};
