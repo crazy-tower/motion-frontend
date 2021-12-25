c3023706c0910e7fdfbe50f5b5e0c739ef7ad508
Keiichi-S <keiichidori1222@gmail.com>
2021-12-25 15:53:26 +0900

    add hand motion function and refactoring

--- components/Room/LocalVideo.tsx
+++ components/Room/LocalVideo.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import { Dispatch, SetStateAction, useEffect, useRef } from 'react'
-import * as handpose from "@tensorflow-models/handpose";
-import { drawHand } from "../../utils/drawHand";
-import '@tensorflow/tfjs-backend-webgl';
+import { runHandpose } from '../../utils/runHandpose'
 
 type Props = {
   setLocalStream: Dispatch<SetStateAction<MediaStream | undefined>>
@@ -10,7 +8,7 @@ type Props = {
 
 const LocalVideo: NextPage<Props> = ({ setLocalStream }) => {
   const localVideoRef = useRef<HTMLVideoElement>(null)
-  const canvasRef = useRef(null);
+  const canvasRef = useRef(null)
 
   useEffect(() => {
     navigator.mediaDevices
@@ -26,65 +24,50 @@ const LocalVideo: NextPage<Props> = ({ setLocalStream }) => {
       .catch((error) => console.error(error))
   }, [localVideoRef, setLocalStream])
 
-  const runHandpose = async () => {
-    const net = await handpose.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
-
-  const detect = async (net) => {
-    // Check data is available
-    if (
-      typeof localVideoRef.current !== "undefined" &&
-      webcamRef.current !== null
-    ) {
-      // Get Video Properties
-      const video = localVideoRef.current;
-      const videoWidth = localVideoRef.current.videoWidth;
-      const videoHeight = localVideoRef.current.videoHeight;
-
-      // Set video width
-      localVideoRef.current.width = videoWidth;
-      localVideoRef.current.height = videoHeight;
-
-      // Set canvas height and width
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
-
-      // Make Detections
-      const hand = await net.estimateHands(video);
-      console.log(hand);
-
-      // Draw mesh
-      const ctx = canvasRef.current.getContext("2d");
-      drawHand(hand, ctx);
-    }
-  };
-
-  runHandpose();
+  runHandpose(localVideoRef, canvasRef)
 
   return (
     <>
-    <video ref={localVideoRef} autoPlay playsInline width={600}></video>
-    <canvas
-          ref={canvasRef}
+      <div
+        style={{
+          position: 'relative',
+          left: 0,
+          right: 0,
+          zindex: 9,
+          width: 600,
+          height: 480,
+        }}
+      >
+        <video
+          ref={localVideoRef}
+          autoPlay
+          playsInline
           style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
+            marginLeft: 'auto',
+            marginRight: 'auto',
             left: 0,
             right: 0,
-            textAlign: "center",
             zindex: 9,
-            width: 640,
+            width: 600,
+            height: 480,
+          }}
+        ></video>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            zindex: 9,
+            width: 600,
             height: 480,
           }}
         />
+      </div>
     </>
-    )
+  )
 }
 
 export default LocalVideo
