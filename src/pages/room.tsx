c76acd8d174c2f7a16f369437bcc63c43541cd36
shellingford330 <pg.econ4869@gmail.com>
2022-01-15 22:23:37 +0900

    feat: intro styled-component

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -3,34 +3,31 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.bgGreen};
+  height: 100vh;
+  padding: 4rem 2rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Page: NextPage = () => {
   const [room, setRoom] = useState<string>();
 
   return (
-    <div
-      style={{
-        padding: '0 2rem',
-        backgroundColor: '#006654',
-      }}
-    >
+    <>
       <Head>
         <title>Room | motion</title>
         <meta name="description" content="Join room" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div
-        style={{
-          height: '100vh',
-          padding: '4rem 0',
-          flex: 1,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <Wrapper>
         <Heading as="h1" size="2xl" marginY="3">
           Join room
         </Heading>
@@ -53,8 +50,8 @@ const Page: NextPage = () => {
             </Button>
           </a>
         </Link>
-      </div>
-    </div>
+      </Wrapper>
+    </>
   );
 };
 
